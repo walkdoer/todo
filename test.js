2a7ec6e207194640e8b0d30df559d51fa8033a86
andrew <zhangmhao@gmail.com>
2012-10-26 23:09:56 +0800

    GET

--- test.js
+++ test.js
@@ -1,37 +1,32 @@
-var app = require('express').createServer(),
-    port = 1337,
-    users = [
-        {name: 'Clonn'},
-        {name: 'Chi'}
-    ];
+var http = require('http'),
+    url  = require('url'),
+    fs   = require("fs"),
+    server;
 
-app.listen(port);
+server = http.createServer(function (req,res) {
+    var urlData,
+        encode   = "utf8",
+        filePath = "view/express_get_example_form.html",
+        action;
 
-app.all('/user/:id/:op?', function(req, res, next){
-    req.user = users[req.params.id];
-    if (req.user) {
-    	console.log('next');
-        next();
-    } else {
-        next(new Error('cannot find user ' + req.params.id));
+    urlData = url.parse(req.url,true);
+    action = urlData.pathname;
+    res.writeHead(200, {"Content-Type":"text/html; charset=utf-8"});
+
+    if (action === "/Signup") {
+       user = urlData.query;
+       res.end("<h1>" + user.username + "歡迎您的加入</h1><p>我們已經將會員啟用信寄至" + user.email + "</p>");
     }
+    else {
+        fs.readFile(filePath, encode, function(err, file) {
+
+            res.write(file);
+            res.end();
+        });
+    }
+        
 });
 
-app.get('/user/:id', function(req, res){
-    res.send('viewing ' + req.user.name);
-});
+server.listen(3000);
 
-app.get('/user/:id/edit', function(req, res){
-    res.send('editing ' + req.user.name);
-});
-
-app.get('/user/:id/delete', function(req, res){
-	console.log('delete');
-    res.send('deleting ' + req.user.name);
-});
-
-app.get('*', function(req, res){
-    res.send('Page not found!', 404);
-});
-
-console.log('start express server\n');
+console.log('Server跑起來了，現在時間是:' + new Date());
