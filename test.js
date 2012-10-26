cb6db5735d2c2bf32e64908006e69d9d68b19148
andrew <zhangmhao@gmail.com>
2012-10-26 23:27:10 +0800

    ajax

--- test.js
+++ test.js
@@ -7,27 +7,34 @@ var http = require('http'),
 server = http.createServer(function (req,res) {
     var urlData,
         encode   = "utf8",
-        filePath = "view/express_get_example_form.html",
+        filePath = "view/express_ajax_example_form.html",
+        formData,
         action;
 
     urlData = url.parse(req.url,true);
     action = urlData.pathname;
-    res.writeHead(200, {"Content-Type":"text/html; charset=utf-8"});
 
     if (action === "/Signup") {
         formData = '';
-	    req.on("data", function (data) {
-	        formData += data;
-	    });
+        req.on("data", function (data) {
 
-	    req.on("end", function () {
-	        user = qs.parse(formData);
-	        res.end("<h1>" + user.username + "歡迎您的加入</h1><p>我們已經將會員啟用信寄至" + user.email + "</p>");
-	    });
+            formData += data;
+
+        });
+
+        req.on("end", function () {
+            var msg;
+
+            user = qs.parse(formData);
+            user.id = "123456";
+            msg = JSON.stringify(user);
+            res.writeHead(200, {"Content-Type":"application/json;","Content-Length":msg.length});
+            res.end(msg);
+        });
     }
     else {
         fs.readFile(filePath, encode, function(err, file) {
-
+            res.writeHead(200, {"Content-Type":"text/html; charset=utf-8"});
             res.write(file);
             res.end();
         });
